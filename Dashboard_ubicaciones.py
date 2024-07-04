7508bd755687b00285698954959d3ff06bdf1bbc
José Arturo Olivares Gallegos <arturoolivaresgallegos@gmail.com>
2024-07-04 08:23:40 -0600

    Update Dashboard_ubicaciones.py

--- Dashboard_ubicaciones.py
+++ Dashboard_ubicaciones.py
@@ -94,7 +94,7 @@ with col1:
 
 with col2:
     st.header('Porcentaje dado de alta')
-    posiciones_bien=filtered_DB['VoF'].sum()
+    posiciones_bien=df['Ubicacion de articulo'].count()
     posiciones=df['Articulo'].count()
     Porcentaje_posiciones=round(((posiciones_bien/posiciones)*100),2)
     maximo=100
