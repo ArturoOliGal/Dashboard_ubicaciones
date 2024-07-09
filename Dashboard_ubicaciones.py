63999f77b1d3d9bd4e6d1c77dd41596324779d59
José Arturo Olivares Gallegos <arturoolivaresgallegos@gmail.com>
2024-07-09 14:10:26 -0600

    Update Dashboard_ubicaciones.py

--- Dashboard_ubicaciones.py
+++ Dashboard_ubicaciones.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.set_page_config(layout='wide')
+
 st.title('Dashboard ubicaciones')
 st.markdown('Pocisiones incorrectas, avance de ubicaciones totales y detallado')
 st.sidebar.header('Filtro de dashboard')
